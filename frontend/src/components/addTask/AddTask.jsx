5af0f1feab7ed56bb13b91f26ba28fe0295f51cb
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-10-23 18:57:12 +0530

    added components add Task, taskdetails cards to the home page with reevent functions, validated the input fields

--- frontend/src/components/addTask/AddTask.jsx
+++ frontend/src/components/addTask/AddTask.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import './AddTask.css'
 
-const AddTask = ({value}) => {
+const AddTask = ({handleTitle,handleDescription,handleAdd,titleErrorMsg,descErrorMsg}) => {
   return (
     <div className='add-task-container'>
         <div className='add-task-heading'>Add a Task</div>
         <div className='add-task-title'>
-            <input type="text" placeholder='Title' value={value}/>
+            <input type="text" placeholder='Title'  onChange={handleTitle}/>
+            {titleErrorMsg}
+            {console.log("title",titleErrorMsg)}
         </div>
         <div className='add-task-description'>
-            <input type="text" placeholder='Description' value={value} />
+            <input type="text" placeholder='Description' onChange={handleDescription} />   
+            {descErrorMsg} 
         </div>
         <div className='add-task-button'>
-            <button>Add</button>
+            <button onClick={handleAdd}>Add</button>
+            
         </div>
     </div>
   )
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,8 +1,96 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AddTask from '../../components/addTask/AddTask'
+import TaskDetailCard from '../../components/taskDetailCard/TaskDetailCard'
+import './Home.css'
 
 const Home = () => {
+  const [title,setTitle] = useState("");
+  const [description, setDescription] = useState("")
+  const [isAdd, setIsAdd] = useState(false);
+  const [error, setError] = useState({})
+
+  const handleTitle = (e) => {
+    setTitle(e.target.value)
+    console.log(title)
+  }
+
+  const handleDescription = (e) => {
+    setDescription(e.target.value)
+    console.log(description)
+  }
+
+  const validation = () => {
+    let err = {tit: "", desc:""};
+    let valid = true;
+    if(!title.trim()){
+      err.tit = "Title is Empty!";
+      valid = false;
+    }
+    if(!description.trim()){
+      err.desc = "Description is Empty!";
+      valid = false;
+      console.log("check")
+    }
+    setError(err);
+    return valid;
+  }
+
+  const handleAdd = () => {
+    const validate = validation();
+    if(validate){
+       setIsAdd(true)
+    }else{
+      setIsAdd(false)
+      console.log("helloo")
+    }
+   
+  }
+
   return (
-    <div>Home</div>
+    <div className='home-container'>
+      <div className='home-add-task'>
+        <AddTask
+          handleTitle={handleTitle}
+          handleDescription={handleDescription}
+          handleAdd={handleAdd}
+          titleErrorMsg={error.tit && <span>{error.tit}</span>}
+          descErrorMsg={error.desc && <span>{error.desc}</span>}
+        />
+      </div>
+      <div className='home-task-card'>
+      {isAdd &&
+        <TaskDetailCard
+          title={title}
+          description={description}
+        />
+      }
+      {isAdd &&
+        <TaskDetailCard
+          title={title}
+          description={description}
+        />
+      }
+      {isAdd &&
+        <TaskDetailCard
+          title={title}
+          description={description}
+        />
+      }
+      {isAdd &&
+        <TaskDetailCard
+          title={title}
+          description={description}
+        />
+      }
+      {isAdd &&
+        <TaskDetailCard
+          title={title}
+          description={description}
+        />
+      }
+      </div>
+      
+    </div>
   )
 }
 
