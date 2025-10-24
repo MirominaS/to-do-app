88bf8d829066969e916893d4df1e4bf2535fe650
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-10-24 20:55:02 +0530

    update function created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
-import TaskDetailCard from './components/taskDetailCard/TaskDetailCard'
 import Home from './pages/home/Home'
 
 function App() {
  return (
-    <div>
-      {/* <Home/> */}
-      <TaskDetailCard/>
+    <div className='app'>
+      <Home/>
     </div>
   )
     
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import AddTask from '../../components/addTask/AddTask'
 import TaskDetailCard from '../../components/taskDetailCard/TaskDetailCard'
+import { v4 as uuidv4 } from 'uuid';
 import './Home.css'
 
 const Home = () => {
@@ -8,6 +9,8 @@ const Home = () => {
   const [description, setDescription] = useState("")
   const [error, setError] = useState({})
   const [listTasks, setListTasks] = useState([]);
+  const [update, setUpdate] = useState(false)
+  const [updatedId, setUpdatedId] = useState(null)
 
   const handleTitle = (e) => {
     setTitle(e.target.value)
@@ -38,13 +41,23 @@ const Home = () => {
   const handleAdd = () => {
     const validate = validation();
     if(validate){
-      const newTask = {
-        id: Date.now(),
+      if(update){
+         setListTasks(listTasks.map(listTask=>
+          listTask.id === updatedId ? {...listTask,title:title,description:description} : listTask
+         )) 
+         setUpdatedId(null)
+         setUpdate(false)
+      }else{
+        const newTask = {
+        id: uuidv4(),
         title:title,
         description: description,
-        done:false
+        done:false,
+        delete:false
       }
       setListTasks([newTask,...listTasks]);
+      }
+      
       setTitle("");
       setDescription(""); 
     }   
@@ -54,6 +67,16 @@ const Home = () => {
       setListTasks(listTasks.map(listTask =>
         listTask.id === id ? {...listTask,done:true} : listTask      
       ))            
+  }
+
+  const handleUpdate = (id) => {
+    setUpdate(true)    
+  }
+
+  const handleDelete = (id) => {
+    setListTasks(listTasks.map(listTask =>
+      listTask.id === id ? {...listTask,delete:true} : listTask      
+    ))  
   }
 
   return (
@@ -67,19 +90,22 @@ const Home = () => {
           valueDescription={description}
           titleErrorMsg={error.tit && <span className='error-msg'>{error.tit}</span>}
           descErrorMsg={error.desc && <span className='error-msg'>{error.desc}</span>}
+          label={update ? "Update" : "Add"}
         />
       </div>
 
       <div className='home-task-card'>
         {listTasks.length > 0 && listTasks.slice(0,5).map((listTask) => 
-          !listTask.done ? (
+          (!listTask.done && !listTask.delete  ) && (
             <TaskDetailCard
               key={listTask.id}
               title={listTask.title}
               description={listTask.description}
-              handleDone={()=>handleDone(listTask.id)}
+              handleDone={()=>{handleDone(listTask.id);console.log("Completed",listTask.title)}}
+              handleUpdate={()=>{handleUpdate(listTask.id);console.log("Updated"),listTask.title}}
+              handleDelete={()=>{handleDelete(listTask.id);console.log("Deleted",listTask.title)}}
             />
-          ): null 
+          )
         )}
       </div>
     </div>
